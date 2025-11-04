76f82badeb1a65cf03453d3d6a23ff04e8feefdc
almircoo <almircoo@proton.me>
2025-11-04 18:43:22 +0000

    register the models in admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ouser
+
 
 # Register your models here.
+@admin.register(Ouser)
+class OuserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "date_joined")
+    list_filter = ("is_active", "is_staff", "is_active", "groups")
+    filter_horizontal = ("groups", "user_permissions")
+    search_fields = ("username", "email")
